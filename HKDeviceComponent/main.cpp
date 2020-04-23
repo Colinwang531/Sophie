7ec78652c17c31e5097f41f9d86b3c668feee69b
Colinwang531 <wangkw531@hotmail.com>
2020-04-23 14:42:19 +0800

    Add glog files

--- HKDeviceComponent/main.cpp
+++ HKDeviceComponent/main.cpp
@@ -1,7 +1,26 @@
-#include <cstdio>
+#ifdef _WINDOWS
+#include "glog/log_severity.h"
+#include "glog/logging.h"
+#else
+#include "glog/log_severity.h"
+#include "glog/logging.h"
+#endif//_WINDOWS
 
-int main()
+int main(int argc, char** argv)
 {
-    printf("hello from HKDeviceComponent!\n");
+	FLAGS_stderrthreshold = INFO;
+	FLAGS_colorlogtostderr = 1;
+	google::InitGoogleLogging(argv[0]);
+	google::SetLogDestination(INFO,
+#ifdef _WINDOWS
+		".\\"
+#else
+		"./"
+#endif
+	);
+
+	LOG(INFO) << "Hello glog ...";
+
+	google::ShutdownGoogleLogging();
     return 0;
 }
